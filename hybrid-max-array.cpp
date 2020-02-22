5cb1069cc0c86d45448ffe36d0b81c28fedc49c4
Ehmad <justEhmadSaeed@gmail.com>
2020-02-22 18:32:37 +0500

    🎯Successful Results

--- hybrid-max-array.cpp
+++ hybrid-max-array.cpp
@@ -19,12 +19,15 @@ struct subArray
     int sum;
 };
 
+subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high);
+subArray HYBRID_FIND_MAX_SUBARRAY(int *A, int low, int high);
 subArray FIND_MAX_SUBARRAY_BRUTE_FORCE(int *A, int low, int high);
 void RandomArray(int array[], int size);
 
 int main()
 {
-    int size = 8;
+
+    int size;
     cout << "Enter the Size of Array: ";
     cin >> size;
     int *A = new int[size];
@@ -34,18 +37,18 @@ int main()
     srand(time(NULL));
     RandomArray(A, size);
 
-    // Code to call Brute Force and measure time duration
+    // Code to call Recursive Max Array and measure time duration
 
     auto start = chrono::high_resolution_clock::now();
     subArray miniArr;
-    miniArr = FIND_MAX_SUBARRAY_BRUTE_FORCE(A, 0, size - 1);
+    miniArr = HYBRID_FIND_MAX_SUBARRAY(A, 0, size - 1);
     auto end = chrono::high_resolution_clock::now();
     double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
     // Code to calculate the time duration
 
     time_taken *= 1e-9;
-    cout << "\n\nTime taken by the Brute Force Algorithm, with " << size << " size of Array is: ";
+    cout << "\n\n Time taken by the Recursive max array, with " << size << " size of Array is: ";
     cout << fixed << time_taken << setprecision(9) << " seconds" << endl;
 
     // Output of the SubArray
@@ -57,7 +60,7 @@ int main()
     // Code to Save array in csv file
 
     ofstream OutData;
-    OutData.open("brute-force-max-array.csv");
+    OutData.open("hybrid-max-array.csv");
     for (int i = 0; i < size; i++)
         OutData << A[i] << endl;
 
@@ -65,12 +68,77 @@ int main()
     return 0;
 }
 
+subArray HYBRID_FIND_MAX_SUBARRAY(int *A, int low, int high)
+{
+    int size = high - low + 1;
+    if (size <= 30)
+
+        return FIND_MAX_SUBARRAY_BRUTE_FORCE(A, low, high);
+
+    else
+    {
+        int mid = floor((high + low) / 2);
+
+        subArray leftArray;
+        subArray rightArray;
+        subArray crossArray;
+
+        leftArray = HYBRID_FIND_MAX_SUBARRAY(A, low, mid);
+
+        rightArray = HYBRID_FIND_MAX_SUBARRAY(A, mid + 1, high);
+
+        crossArray = FIND_MAX_CROSSING_SUBARRAY(A, low, mid, high);
+
+        if (leftArray.sum >= rightArray.sum && leftArray.sum >= crossArray.sum)
+            return leftArray;
+        else if (rightArray.sum >= leftArray.sum && rightArray.sum >= crossArray.sum)
+            return rightArray;
+        else
+            return crossArray;
+    }
+}
+
+subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high)
+{
+    int left_sum = A[mid], sum, max_left = mid;
+
+    sum = A[mid];
+
+    for (int i = mid - 1; i >= low; --i)
+    {
+        sum += A[i];
+        if (sum > left_sum)
+        {
+            left_sum = sum;
+            max_left = i;
+        }
+    }
+    sum = A[mid + 1];
+    int max_right = mid + 1, right_sum = A[mid + 1];
+
+    for (int j = mid + 2; j <= high; ++j)
+    {
+        sum += A[j];
+        if (sum > right_sum)
+        {
+            right_sum = sum;
+            max_right = j;
+        }
+    }
+
+    subArray miniArray;
+    miniArray.max_left = max_left;
+    miniArray.max_right = max_right;
+    miniArray.sum = left_sum + right_sum;
+
+    return miniArray;
+}
+
 subArray FIND_MAX_SUBARRAY_BRUTE_FORCE(int *A, int low, int high)
 {
     int max_sum = A[low];
     int size = high - low + 1;
-
-    if (high != low)
+    if (low != high)
     {
         for (int i = 0; i < size; ++i)
         {
@@ -98,6 +166,7 @@ subArray FIND_MAX_SUBARRAY_BRUTE_FORCE(int *A, int low, int high)
 // RandomArray Function to fill the array with random numbers
 void RandomArray(int array[], int size)
 {
+
     for (int i = 0; i < size; i++)
     {
         array[i] = rand();
--- recursive-max-array.cpp
+++ recursive-max-array.cpp
@@ -5,17 +5,20 @@
 #include <math.h>
 #include <iomanip>
 #include <cstdlib>
-//#include <chrono>
+#include <chrono>
 #include <time.h>
 #include <fstream>
 
 using namespace std;
+
+// Struct to store values
 struct subArray
 {
     int max_left;
     int max_right;
     int sum;
 };
+
 subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high);
 subArray FIND_MAX_SUBARRAY(int *A, int low, int high);
 void RandomArray(int array[], int size);
@@ -23,30 +26,34 @@ void RandomArray(int array[], int size);
 int main()
 {
 
-    int size = 16;
+    int size = 8;
     cout << "Enter the Size of Array: ";
-    cin >> size;
-    int *A = new int[size];
+    //cin >> size;
+    int arr[8] = {-2, -5, 6, -2, -3, 1, 5, -6};
+    int *A = arr;
 
     // To generate random Numbers and store them
 
     srand(time(NULL));
-    RandomArray(A, size);
+    //RandomArray(A, size);
+
     // Code to call Recursive Max Array and measure time duration
 
-    //auto start = chrono::high_resolution_clock::now();
+    auto start = chrono::high_resolution_clock::now();
     subArray miniArr;
     miniArr = FIND_MAX_SUBARRAY(A, 0, size - 1);
-    //auto end = chrono::high_resolution_clock::now();
-    //double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+    auto end = chrono::high_resolution_clock::now();
+    double time_taken = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
 
     // Code to calculate the time duration
 
-    /* time_taken *= 1e-9;
-    cout << "\n Time taken by the recursive max array, with " << size << " size of Array is: ";
-    cout << fixed << time_taken << setprecision(9) << " seconds" << endl; */
+    time_taken *= 1e-9;
+    cout << "\n\n Time taken by the Recursive max array, with " << size << " size of Array is: ";
+    cout << fixed << time_taken << setprecision(9) << " seconds" << endl;
 
-    cout << "Left Index is " << miniArr.max_left << endl;
+    // Output of the SubArray
+
+    cout << "\nLeft Index is " << miniArr.max_left << endl;
     cout << "Right Index is " << miniArr.max_right << endl;
     cout << "Sum is " << miniArr.sum << endl;
 
@@ -63,7 +70,6 @@ int main()
 
 subArray FIND_MAX_SUBARRAY(int *A, int low, int high)
 {
-    int left_sum = -14748369, sum = 0, max_left, max_right, right_sum = -1474836;
     if (high == low)
     {
         subArray miniArray;
@@ -77,20 +83,20 @@ subArray FIND_MAX_SUBARRAY(int *A, int low, int high)
         int mid = floor((high + low) / 2);
 
         subArray leftArray;
+        subArray rightArray;
+        subArray crossArray;
+
         leftArray = FIND_MAX_SUBARRAY(A, low, mid);
 
-        subArray rightArray;
         rightArray = FIND_MAX_SUBARRAY(A, mid + 1, high);
 
-        subArray crossArray;
         crossArray = FIND_MAX_CROSSING_SUBARRAY(A, low, mid, high);
+
 
         if (leftArray.sum >= rightArray.sum && leftArray.sum >= crossArray.sum)
             return leftArray;
-
         else if (rightArray.sum >= leftArray.sum && rightArray.sum >= crossArray.sum)
             return rightArray;
-
         else
             return crossArray;
     }
@@ -98,8 +104,11 @@ subArray FIND_MAX_SUBARRAY(int *A, int low, int high)
 
 subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high)
 {
-    int left_sum = -10000, sum = 0, max_left, max_right, right_sum = -10000;
-    for (int i = mid; i >= low; --i)
+    int left_sum = A[mid], sum, max_left = mid;
+
+    sum = A[mid];
+
+    for (int i = mid - 1; i >= low; --i)
     {
         sum += A[i];
         if (sum > left_sum)
@@ -108,8 +117,10 @@ subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high)
             max_left = i;
         }
     }
-    sum = 0;
-    for (int j = mid + 1; j <= high; ++j)
+    sum = A[mid + 1];
+    int max_right = mid + 1, right_sum = A[mid + 1];
+
+    for (int j = mid + 2; j <= high; ++j)
     {
         sum += A[j];
         if (sum > right_sum)
@@ -122,7 +133,7 @@ subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high)
     subArray miniArray;
     miniArray.max_left = max_left;
     miniArray.max_right = max_right;
-    miniArray.sum = sum;
+    miniArray.sum = left_sum + right_sum;
 
     return miniArray;
 }
@@ -130,6 +141,11 @@ subArray FIND_MAX_CROSSING_SUBARRAY(int *A, int low, int mid, int high)
 // RandomArray Function to fill the array with random numbers
 void RandomArray(int array[], int size)
 {
+
     for (int i = 0; i < size; i++)
+    {
         array[i] = rand();
+        if (i % 2 == 0)
+            array[i] *= -1;
+    }
 }
